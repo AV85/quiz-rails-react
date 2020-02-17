65f8d192e45dd7dde6586133e26143a0797db6cc
AV85 <sasha85ua@gmail.com>
2020-02-17 23:16:33 +0200

    added components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../hoc/Layout/Layout'
 import Quiz from "../containers/Quiz/Quiz";
+import ActiveQuiz from "./ActiveQuiz/ActiveQuiz";
 
 class App extends React.Component {
   render() {
--- app/javascript/containers/Quiz/Quiz.js
+++ app/javascript/containers/Quiz/Quiz.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react'
 import classes from './Quiz.module.sass'
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz"
 
 class Quiz extends Component {
+    state = {
+      quiz: [
+          {
+              answers: [
+                  {text: 'question1'},
+                  {text: 'question2'},
+                  {text: 'question3'},
+                  {text: 'question4'},
+                  {text: 'question5'}
+              ]
+          }
+      ]
+    };
+
     render() {
         return (
             <div className={classes.Quiz}>
-                <h1>Quiz</h1>
+                <div className={classes.QuizWrapper}>
+                    <h1>Quiz</h1>
+                    <ActiveQuiz
+                        answers={this.state.quiz[0].answers}
+                    />
+                </div>
             </div>
         );
     }
