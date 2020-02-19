cc3cdd479f28c2cad55a8b793e195c9398381427
AV85 <sasha85ua@gmail.com>
2020-02-19 23:08:34 +0200

    added logig in components

--- app/javascript/components/ActiveQuiz/ActiveQuiz.js
+++ app/javascript/components/ActiveQuiz/ActiveQuiz.js
@@ -6,12 +6,13 @@ const ActiveQuiz = props => (
   <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
       <span>
-        <strong>1.</strong>&nbsp;
+        <strong>{props.answerNumber}.</strong>&nbsp;
         {props.question}
         </span>
-        <small>1 from 10</small>
+        <small>{props.answerNumber} from {props.quizLength}</small>
     </p>
       <AnswersList
+        state={props.state}
         answers = {props.answers}
         onAnswerClick={props.onAnswerClick}
       />
--- app/javascript/containers/Quiz/Quiz.js
+++ app/javascript/containers/Quiz/Quiz.js
@@ -4,10 +4,13 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz"
 
 class Quiz extends Component {
   state = {
+    activeQuestion: 0,
+    answerState: null,
     quiz: [
       {
         question: 'What will be the result 2 + 2 ?',
         rightAnswerId: 2,
+        id: 1,
         answers: [
           {text: '3', id: 1},
           {text: '4', id: 2},
@@ -15,13 +18,51 @@ class Quiz extends Component {
           {text: '6', id: 4},
           {text: 'I don\'t know', id: 5}
         ]
+      },
+      {
+        question: 'What will be the result 10 + 10 ?',
+        rightAnswerId: 3,
+        id: 2,
+        answers: [
+          {text: '10', id: 1},
+          {text: '15', id: 2},
+          {text: '20', id: 3},
+          {text: '30', id: 4},
+          {text: 'I don\'t know', id: 5}
+        ]
       }
     ]
   };
 
   onAnswerClickHandler = (answerId) => {
-    console.log('Answer Id: ', answerId)
+    const question = this.state.quiz[this.state.activeQuestion];
+
+    if (question.rightAnswerId === answerId) {
+      this.setState({
+        answerState: {[answerId]: 'success'}
+      })
+      const timeout = window.setTimeout(() => {
+        if (this.isQuizFinished()) {
+          console.log("Finished")
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null
+          })
+        }
+        window.clearTimeout(timeout)
+      }, 1000);
+
+    } else {
+      this.setState({
+        answerState: {[answerId]: 'error' }
+      })
+    }
   };
+
+  isQuizFinished(){
+    return this.state.activeQuestion + 1 === this.state.quiz.length
+  }
 
   render() {
     return (
@@ -29,9 +70,12 @@ class Quiz extends Component {
         <div className={classes.QuizWrapper}>
           <h1>Quiz</h1>
           <ActiveQuiz
-            answers={this.state.quiz[0].answers}
-            question={this.state.quiz[0].question}
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            question={this.state.quiz[this.state.activeQuestion].question}
             onAnswerClick={this.onAnswerClickHandler}
+            quizLength={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
+            state={this.state.answerState}
           />
         </div>
       </div>
