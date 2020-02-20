3285e0674f2ec5e9ec14e25afb26ca430686383e
AV85 <sasha85ua@gmail.com>
2020-02-20 22:12:06 +0200

    added components

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'react-rails'
 gem 'rubocop', require: false
 gem 'webpacker'
+gem "font-awesome-rails"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/javascript/components/FinishedQuiz/FinishedQuiz.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import classes from './FinishedQuiz.module.sass'
+import Button from "../UI/Button/Button";
+
+const FinishedQuiz = props => {
+
+  const successCount = Object.keys(props.results).reduce((total, key) => {
+    if (props.results[key] === 'success') {
+      total++
+    }
+    return total
+  }, 0);
+
+  return(
+    <div className={classes.FinishedQuiz}>
+      <h1>Finished!!!!!!!</h1>
+      <ul>
+        { props.quiz.map((quizItem, index) => {
+          const cls = [
+            'fa',
+            props.results[quizItem.id] === 'error' ? 'fa-times error' : 'fa-check success',
+            classes[props.results[quizItem.id]]
+          ];
+          return(
+            <li key={index}
+            >
+              <strong>{index + 1}</strong>.&nbsp;
+              {quizItem.question}
+              <i className={cls.join(' ')}/>
+            </li>
+          )
+        }) }
+      </ul>
+      <p>Correct {successCount} from {props.quiz.length}</p>
+      <div>
+        <Button onClick={props.onRetry} type='primary'>Replay?</Button>
+        <Button type='success'>Go to tests</Button>
+      </div>
+    </div>
+  )
+};
+
+export default FinishedQuiz
